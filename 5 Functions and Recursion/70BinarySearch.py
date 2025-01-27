e6cb16d1c4270477e2e8ae56ba7deaf6814232f2
Prince Vaviya <1princevaviya@gmail.com>
2025-01-27 10:07:55 +0530

    Functions and Recursion

--- 5 Functions and Recursion/70BinarySearch.py
+++ 5 Functions and Recursion/70BinarySearch.py
@@ -1,13 +1,15 @@
 def binary_search(arr, target, low, high):
-    
+
     mid = (low + high) // 2
 
     if arr[mid] == target:
         return mid  
     elif arr[mid] < target:
         return binary_search(arr, target, mid + 1, high) 
+    elif arr[mid] > target:
+        return binary_search(arr, target, low, mid - 1)
     else:
-        return binary_search(arr, target, low, mid - 1) 
+        return -1 
 
 
 
@@ -16,7 +18,7 @@ target = int(input("Enter the number to search for: "))
 
 result = binary_search(sorted_list, target, 0, len(sorted_list) - 1)
 
-if result != -1:
-    print(f"Element {target} found at index: {result}")
-else:
+if result == -1:
     print(f"Element {target} not found in the list.")
+else:
+    print(f"Element {target} found at index: {result}")
